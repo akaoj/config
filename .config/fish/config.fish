5c5303f3b9b517543e58aac3203a28f7469d55b6
Valentin Bremond <valentin.bremond@displayce.com>
2019-02-27 10:49:50 +0100

    Make the git log diff user origin as base
    `origin` being the source of truth, we want to check the changes against
    it, not against the local `master`.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,8 +11,8 @@ abbr -a gb git branch
 abbr -a gd git diff
 abbr -a gdw git dw
 abbr -a gaa git add --all
-abbr -a gl "git log master.."  # display commits added on top of master
 abbr -a gap "git add -N --all ;and git add --all --patch"
+abbr -a gl "git log origin/master.."  # display commits added on top of master
 abbr -a gcd "cd (git rev-parse --show-toplevel)"  # cd in the root folder of the directory
 abbr -a gu "git fetch --tags --prune --all"
 abbr -a gv "git diff HEAD --name-only --relative | xargs nvim"  # open all modified files in neovim
